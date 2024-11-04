1599f3a0c760970741ba20f9f383bf25c1ee4ff6
pehgamarra <pedrohhouro@Gmail.com>
2024-11-04 13:15:44 -0300

    Error fix: Logout

--- GamarraFinance/Finance/urls.py
+++ GamarraFinance/Finance/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import register, user_login, logged_in
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', user_login, name='login'),
     path('logged_in/', logged_in, name='logged_in'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
